11cf4beda879f21f2a67d3730c2af79e0adfed7f
Muratbekova Muragul <47049297+Muragul@users.noreply.github.com>
2021-08-12 13:51:38 +0600

    [feature] Created start steps pages

---
+++ app/src/main/java/com/example/envelope/ui/payment/PaymentsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.envelope.ui.payment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.envelope.R
+
+class PaymentsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payments)
+    }
+}
