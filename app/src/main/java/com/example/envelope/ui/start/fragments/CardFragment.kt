bb28c88669ffad1d6fd5eef4193bb5c51555bb98
Orken Kyrgaul <kyrgaulorken@gmail.com>
2021-08-20 12:27:47 +0600

    [feature] add card adding

--- app/src/main/java/com/example/envelope/ui/start/fragments/CardFragment.kt
+++ app/src/main/java/com/example/envelope/ui/start/fragments/CardFragment.kt
@@ -2,10 +2,13 @@ package com.example.envelope.ui.start.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.envelope.databinding.FragmentCardBinding
 import com.example.envelope.ui.start.StartActivity
 import com.example.envelope.utils.COMPLETE_TAG
 import com.example.envelope.utils.binding.BindingFragment
+import com.redmadrobot.inputmask.MaskedTextChangedListener
+import com.redmadrobot.inputmask.helper.AffinityCalculationStrategy
 
 class CardFragment : BindingFragment<FragmentCardBinding>(FragmentCardBinding::inflate) {
 
@@ -20,12 +23,42 @@ class CardFragment : BindingFragment<FragmentCardBinding>(FragmentCardBinding::i
                     (activity as StartActivity).restart()
                 }
                 btnNext.setOnClickListener {
-                    (activity as StartActivity).changeFragment(
+                    if (
+                        checkCardNumber(etCardNumber.text.toString()) &&
+                        checkFullName(etCardOwnerName.text.toString()) &&
+                        checkDate(etCardExpireDate.text.toString()) &&
+                        checkCvv(etCardCvv.text.toString())
+                    ) (activity as StartActivity).changeFragment(
                         CompleteFragment(),
                         COMPLETE_TAG
-                    )
+                    ) else
+                        Toast.makeText(context, "Заполните все поля", Toast.LENGTH_SHORT).show()
                 }
             }
         }
+        initViews()
     }
+
+    private fun initViews() {
+        MaskedTextChangedListener.installOn(
+            binding.etCardNumber,
+            "[0000] [0000] [0000] [0000]",
+            arrayListOf("[0000] [0000] [0000] [0000]"),
+            AffinityCalculationStrategy.WHOLE_STRING
+        )
+        MaskedTextChangedListener.installOn(
+            binding.etCardExpireDate,
+            "[00]{/}[00]",
+            arrayListOf("[00]{/}[00]"),
+            AffinityCalculationStrategy.WHOLE_STRING
+        )
+    }
+
+    private fun checkCardNumber(cardNumber: String): Boolean = cardNumber.length == 19
+    private fun checkFullName(fullName: String): Boolean = fullName.isNotEmpty()
+    private fun checkDate(date: String): Boolean =
+        if (date.length != 5) false
+        else date.substring(0, date.indexOf("/")).toInt() < 13
+
+    private fun checkCvv(cvv: String): Boolean = cvv.length == 3
 }
