7e4ddb3fc68f1aa93174cc6b4c17f273e0b79224
Orken Kyrgaul <kyrgaulorken@gmail.com>
2021-08-25 16:55:03 +0600

    [feature] fix home recycler padding

--- app/src/main/java/com/example/envelope/ui/start/card/CardFragment.kt
+++ app/src/main/java/com/example/envelope/ui/start/card/CardFragment.kt
@@ -2,8 +2,6 @@ package com.example.envelope.ui.start.card
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import com.example.envelope.R
 import com.example.envelope.databinding.FragmentCardBinding
 import com.example.envelope.ui.start.StartActivity
 import com.example.envelope.ui.start.complete.CompleteFragment
@@ -33,12 +31,10 @@ class CardFragment : BindingFragment<FragmentCardBinding>(FragmentCardBinding::i
     }
 
     private fun goToCompletion() {
-        if (validateFields())
-            (activity as StartActivity).showFragment(
-                CompleteFragment(),
-                COMPLETE_TAG
-            ) else
-            Toast.makeText(context, R.string.fill_empty_fields, Toast.LENGTH_SHORT).show()
+        (activity as StartActivity).showFragment(
+            CompleteFragment(),
+            COMPLETE_TAG
+        )
     }
 
     private fun validateFields(): Boolean {
