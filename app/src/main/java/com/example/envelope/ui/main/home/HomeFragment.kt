8b0bcab35c0df3aa0b60a2ef2df50117926d2369
Orken Kyrgaul <kyrgaulorken@gmail.com>
2021-09-16 13:37:46 +0600

    [feature] relayshow

--- app/src/main/java/com/example/envelope/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/example/envelope/ui/main/home/HomeFragment.kt
@@ -4,73 +4,76 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
-import android.util.DisplayMetrics
-import android.view.Gravity
+import android.util.Log
 import android.view.View
-import android.view.WindowManager
 import androidx.annotation.RequiresApi
-import androidx.core.content.ContextCompat
 import com.example.envelope.R
 import com.example.envelope.databinding.FragmentHomeBinding
 import com.example.envelope.ui.ContainerActivity
+import com.example.envelope.ui.main.home.hints.BalloonFactory
 import com.example.envelope.utils.*
 import com.example.envelope.utils.binding.BindingFragment
 import com.example.envelope.utils.extensions.show
 import com.example.envelope.utils.navigation.Screen
-import com.skydoves.balloon.Balloon
-import com.skydoves.balloon.BalloonAnimation
-import com.skydoves.balloon.BalloonSizeSpec
-import com.skydoves.balloon.createBalloon
+import com.skydoves.balloon.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
 
     private lateinit var prefs: SharedPreferences
+    private lateinit var budgetHint: Balloon
+    private lateinit var unexpectedHint: Balloon
+    private lateinit var savingsHint: Balloon
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        prefs = context?.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)!!
-//        prefs.edit().clear().apply()
+        prefs = context?.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)!!
+        prefs.edit().clear().apply()
         initViews()
-//        if (!prefs.contains("hintShowed")) {
-//            startHint()
-//        }
-        setupListeners()
-    }
+        binding.run {
+            val locationArrayBudget = IntArray(2)
+            ivBudgetHint.getLocationOnScreen(locationArrayBudget)
+            budgetHint = BalloonFactory(
+                ivUnexpectedHint,
+                getString(R.string.unexpected_hint),
+                ivBudgetHint.x.toInt()
+            )
+                .create(ivUnexpectedHint.context, this@HomeFragment)
 
-//    private fun startHint() {
-//        binding.run {
-//            val locationArrayBudget = IntArray(2)
-//            ivBudgetHint.getLocationOnScreen(locationArrayBudget)
-//            val budgetHint = createToolTip(
-//                ivBudgetHint,
-//                getString(R.string.budget_hint),
-//                locationArrayBudget[0]
-//            )
-//
-//            val locationArrayExpenses = IntArray(2)
-//            ivUnexpectedHint.getLocationOnScreen(locationArrayExpenses)
-//            val unexpectedHint = createToolTip(
-//                ivUnexpectedHint,
-//                getString(R.string.unexpected_hint),
-//                locationArrayExpenses[0]
-//            )
-//
-//            val locationArraySavings = IntArray(2)
-//            ivSavingsHint.getLocationOnScreen(locationArraySavings)
-//            val savingsHint = createToolTip(
-//                ivSavingsHint,
-//                getString(R.string.savings_hint),
-//                locationArraySavings[0]
-//            )
-//            budgetHint.relayShowAlignBottom(unexpectedHint, ivUnexpectedHint)
-//                .relayShowAlignBottom(savingsHint, ivSavingsHint)
-//            budgetHint.showAlignBottom(ivBudgetHint)
-//            prefs.edit().putBoolean("hintShowed", true).apply()
-//        }
-//    }
+            val locationArrayExpenses = IntArray(2)
+            ivUnexpectedHint.getLocationOnScreen(locationArrayExpenses)
+            unexpectedHint = BalloonFactory(
+                ivUnexpectedHint,
+                getString(R.string.unexpected_hint),
+                locationArrayExpenses[0]
+            )
+                .create(ivUnexpectedHint.context, this@HomeFragment)
+
+            val locationArraySavings = IntArray(2)
+            ivSavingsHint.getLocationOnScreen(locationArraySavings)
+            savingsHint = BalloonFactory(
+                ivSavingsHint,
+                getString(R.string.savings_hint),
+                locationArraySavings[0]
+            )
+                .create(ivSavingsHint.context, this@HomeFragment)
+
+            if (!prefs.contains("hintShowed")) {
+                Log.d("asdasd", "asdadasd")
+                ivBudgetHint.performClick()
+                budgetHint.relayShowAlignBottom(unexpectedHint, ivUnexpectedHint)
+                    .relayShowAlignBottom(savingsHint, ivSavingsHint)
+                ivBudgetHint.performClick()
+                ivBudgetHint.setOnClickListener {
+                    budgetHint.showAlignBottom(ivBudgetHint)
+                }
+                prefs.edit().putBoolean("hintShowed", true).apply()
+            }
+            setupListeners()
+        }
+    }
 
     private fun setupListeners() {
         binding.run {
@@ -86,33 +89,26 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(FragmentHomeBinding::i
             ivBudgetHint.setOnClickListener {
                 val locationArray = IntArray(2)
                 ivBudgetHint.getLocationOnScreen(locationArray)
-                createToolTip(
-                    binding.ivBudgetHint,
-                    getString(R.string.budget_hint),
-                    locationArray[0]
-                ).showAlignBottom(
-                    ivBudgetHint
-                )
+                budgetHint
+                    .showAlignBottom(
+                        ivBudgetHint
+                    )
             }
             ivUnexpectedHint.setOnClickListener {
                 val locationArray = IntArray(2)
                 ivUnexpectedHint.getLocationOnScreen(locationArray)
-                createToolTip(
-                    binding.ivUnexpectedHint,
-                    getString(R.string.unexpected_hint),
-                    locationArray[0]
-                ).showAlignBottom(ivUnexpectedHint)
+                unexpectedHint
+                    .showAlignBottom(
+                        ivUnexpectedHint
+                    )
             }
             ivSavingsHint.setOnClickListener {
                 val locationArray = IntArray(2)
                 ivSavingsHint.getLocationOnScreen(locationArray)
-                createToolTip(
-                    binding.ivSavingsHint,
-                    getString(R.string.savings_hint),
-                    locationArray[0]
-                ).showAlignBottom(
-                    ivSavingsHint
-                )
+                savingsHint
+                    .showAlignBottom(
+                        ivSavingsHint
+                    )
             }
         }
     }
@@ -171,32 +167,37 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(FragmentHomeBinding::i
     }
 
 
-    private fun createToolTip(view: View, hintText: String, x: Int): Balloon {
-        val vm = context?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        val metrics = DisplayMetrics()
-        vm.defaultDisplay.getMetrics(metrics)
-        val tooltipWidth = metrics.widthPixels
-        val arrowLocation: Double = x.toDouble() / (tooltipWidth.toDouble() - 60.toDouble())
-        return createBalloon(view.context) {
-            setArrowSize(10)
-            setWidth(tooltipWidth)
-            setHeight(BalloonSizeSpec.WRAP)
-            setArrowPosition(arrowLocation.toFloat())
-            setArrowSize(20)
-            setArrowDrawable(ContextCompat.getDrawable(view.context, R.drawable.tool_tip_arrow))
-            setCornerRadius(4f)
-            setText(hintText)
-            setTextSize(14f)
-            setTextGravity(Gravity.START)
-            setTextColorResource(R.color.black)
-            setPadding(12)
-            setMarginTop(0)
-            setMarginHorizontal(15)
-            setBackgroundColorResource(R.color.light_blue)
-            setBalloonAnimation(BalloonAnimation.FADE)
-            setLifecycleOwner(lifecycleOwner)
-        }
-    }
+//    private fun createToolTip(view: View, hintText: String, x: Int): Balloon {
+//        val vm = context?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+//        val metrics = DisplayMetrics()
+//        vm.defaultDisplay.getMetrics(metrics)
+//        val tooltipWidth = metrics.widthPixels
+//        val arrowLocation: Double = x.toDouble() / (tooltipWidth.toDouble() - 60.toDouble())
+//        return createBalloon(view.context) {
+//            setArrowSize(10)
+//            setWidth(tooltipWidth)
+//            setHeight(BalloonSizeSpec.WRAP)
+//            setArrowPosition(arrowLocation.toFloat())
+//            setArrowSize(20)
+//            setArrowDrawable(ContextCompat.getDrawable(view.context, R.drawable.tool_tip_arrow))
+//            setCornerRadius(4f)
+//            setText(hintText)
+//            setTextSize(14f)
+//            setTextGravity(Gravity.START)
+//            setTextColorResource(R.color.black)
+//            setPadding(12)
+//            setMarginTop(0)
+//            setMarginHorizontal(15)
+//            setBackgroundColorResource(R.color.light_blue)
+//            setBalloonAnimation(BalloonAnimation.FADE)
+//            setLifecycleOwner(lifecycleOwner)
+//            setIsVisibleOverlay(true) // sets the visibility of the overlay for highlighting an anchor.
+//            setOverlayColorResource(R.color.overlay) // background color of the overlay using a color resource.
+//            setOverlayPadding(6f) // sets a padding value of the overlay shape internally.
+//            setBalloonOverlayAnimation(BalloonOverlayAnimation.FADE) // default is fade.
+//            setDismissWhenOverlayClicked(false) // disable dismissing the balloon when the overlay is clicked.
+//        }
+//    }
 
     private fun openServices() {
         val bundle = Bundle()
